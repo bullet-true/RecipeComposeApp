bc26aef7115886b005eb213897f7d0bc027fe7ab
Igor Fedorov <bullet-1@yandex.ru>
2026-01-17 00:02:53 +0500

    RCA-08: добавил preview BottomNavigation с темной темой

--- app/src/main/java/com/ifedorov/recipecomposeapp/core/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/ifedorov/recipecomposeapp/core/ui/navigation/BottomNavigation.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ifedorov.recipecomposeapp.R
@@ -105,6 +106,17 @@ private fun NavigationButton(
 @Preview(showBackground = true, widthDp = 360)
 @Composable
 private fun PreviewBottomNavigation() {
+    RecipeComposeAppTheme {
+        BottomNavigation(
+            onCategoriesClick = {},
+            onFavoriteClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewBottomNavigationNightMode() {
     RecipeComposeAppTheme {
         BottomNavigation(
             onCategoriesClick = {},
