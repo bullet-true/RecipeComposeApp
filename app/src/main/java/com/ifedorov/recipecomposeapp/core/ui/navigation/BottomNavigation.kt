0ca8743769c8ec110810baf387b62aa20b0890db
Igor Fedorov <bullet-1@yandex.ru>
2026-02-07 20:40:39 +0500

    RCA-20: управление состоянием в Compose

--- app/src/main/java/com/ifedorov/recipecomposeapp/core/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/ifedorov/recipecomposeapp/core/ui/navigation/BottomNavigation.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -96,7 +97,8 @@ private fun NavigationButton(
                 Icon(
                     painter = icon,
                     contentDescription = null,
-                    tint = textColor
+                    tint = textColor,
+                    modifier = Modifier.size(24.dp)
                 )
             }
         }
