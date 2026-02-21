3c6fb759c6e6f8d742ffb3b76c43b770e0206be5
Igor Fedorov <bullet-1@yandex.ru>
2026-02-21 19:49:16 +0500

    RCA-31: первый сетевой запрос (URLConnection)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     id("kotlin-parcelize")
 }
 
@@ -69,4 +70,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
+
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/ifedorov/recipecomposeapp/data/model/CategoryDto.kt
+++ app/src/main/java/com/ifedorov/recipecomposeapp/data/model/CategoryDto.kt
@@ -1,5 +1,8 @@
 package com.ifedorov.recipecomposeapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CategoryDto(
     val id: Int,
     val title: String,
